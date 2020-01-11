30b135c4cbe69a6b3f6d5c722ea28e808950a1a6
Aimee Rivers <airi@dr.dk>
2020-01-11 22:13:07 +0100

    Test fuel_needed_for_mass.

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+fn fuel_needed_for_mass(mass: i32) -> i32 {
+  (mass / 3) - 2
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_with_mass_12() {
+    assert_eq!(2, fuel_needed_for_mass(12));
+  }
+
+  #[test]
+  fn test_with_mass_14() {
+    assert_eq!(2, fuel_needed_for_mass(14));
+  }
+
+  #[test]
+  fn test_with_mass_1969() {
+    assert_eq!(654, fuel_needed_for_mass(1969));
+  }
+
+  #[test]
+  fn test_with_mass_100756() {
+    assert_eq!(33583, fuel_needed_for_mass(100756));
+  }
+}
