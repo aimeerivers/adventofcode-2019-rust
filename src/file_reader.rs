8105d1df4cc8ca53e6b9684c2ee8e61f25b18586
Aimee Rivers <airi@dr.dk>
2020-01-12 19:49:22 +0100

    File reader can parse comma separated usize.

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -8,6 +8,14 @@ pub fn read_vec_i64_from_file(filename: &str) -> Vec<i64> {
   output
 }
 
+pub fn read_vec_usize_from_file(filename: &str) -> Vec<usize> {
+  let input = read_to_string(filename).unwrap();
+  let output: Vec<usize> = input.split(",")
+    .map(|s: &str| s.parse::<usize>().unwrap())
+    .collect();
+  output
+}
+
 #[cfg(test)]
 mod test {
   use super::*;
@@ -16,6 +24,12 @@ mod test {
   fn test_read_vec_i64_from_file() {
     let expected: Vec<i64> = vec![12, 14, 1969, 100756];
     assert_eq!(expected, read_vec_i64_from_file("fixtures/day1.txt"));
+  }
+
+  #[test]
+  fn test_read_vec_usize_from_file() {
+    let expected: Vec<usize> = vec![30,1,1,4,2,5,6,0,99];
+    assert_eq!(expected, read_vec_usize_from_file("fixtures/day2.txt"));
   }
 
 }
