e83f0ae7941b56ef31a7a0f67b2f38c27adc808c
Aimee Rivers <airi@dr.dk>
2020-01-12 15:56:17 +0100

    Woo, total fuel needed calculated with passing a vector!

--- src/fuel_counter_upper.rs
+++ src/fuel_counter_upper.rs
@@ -2,27 +2,43 @@ pub fn fuel_needed_for_mass(mass: i32) -> i32 {
   (mass / 3) - 2
 }
 
+pub fn total_fuel_needed_for_masses(masses: Vec<i32>) -> i32 {
+  let mut total: i32 = 0;
+  for mass in masses.iter() {
+    total += fuel_needed_for_mass(*mass);
+  }
+  total
+}
+
 #[cfg(test)]
-mod tests {
+mod test {
   use super::*;
 
   #[test]
-  fn test_with_mass_12() {
+  fn test_fuel_needed_for_mass_12() {
     assert_eq!(2, fuel_needed_for_mass(12));
   }
 
   #[test]
-  fn test_with_mass_14() {
+  fn test_fuel_needed_for_mass_14() {
     assert_eq!(2, fuel_needed_for_mass(14));
   }
 
   #[test]
-  fn test_with_mass_1969() {
+  fn test_fuel_needed_for_mass_1969() {
     assert_eq!(654, fuel_needed_for_mass(1969));
   }
 
   #[test]
-  fn test_with_mass_100756() {
+  fn test_fuel_needed_for_mass_100756() {
     assert_eq!(33583, fuel_needed_for_mass(100756));
   }
+
+  #[test]
+  fn test_total_fuel_needed_for_masses() {
+    let masses: Vec<i32> = vec![12, 14, 1969, 100756];
+    let expected: i32 = 2 + 2 + 654 + 33583;
+    assert_eq!(expected, total_fuel_needed_for_masses(masses));
+  }
+
 }
