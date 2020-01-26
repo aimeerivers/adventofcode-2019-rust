dba05bb2d949fd30094f197ddb9cea3da45c687b
Aimee Rivers <airi@dr.dk>
2020-01-26 12:47:41 +0100

    Clone the vector instead of reading it every time.
    Thanks tocklime :)

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,11 @@ fn day2_part2() {
   let mut noun = 0;
   let mut verb = 0;
   let goal = 19690720;
+  let input: Vec<usize> = file_reader::read_vec_usize_from_file("input/day2.txt");
 
   'outer_loop: for i in 0..100 {
     for j in 0..100 {
-      let mut test: Vec<usize> = file_reader::read_vec_usize_from_file("input/day2.txt");
+      let mut test = input.clone();
       test[1] = i;
       test[2] = j;
       let result: Vec<usize> = intcode_computer::run_program(test);
