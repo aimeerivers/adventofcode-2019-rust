364e873b39363e50ccc90904777f07a4049164a9
Aimee Rivers <airi@dr.dk>
2020-01-26 13:15:18 +0100

    Remove code from previous days.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,11 @@
 mod file_reader;
-mod fuel_counter_upper;
 mod intcode_computer;
 
 fn main() {
-  day1_part1();
-  day1_part2();
-  day2_part1();
-  day2_part2();
+  solve();
 }
 
-fn day1_part1() {
-  let masses: Vec<i64> = file_reader::read_vec_i64_from_file("input/day1.txt");
-  assert_eq!(3334297, fuel_counter_upper::total_fuel_needed_for_masses(masses));
-}
-
-fn day1_part2() {
-  let masses: Vec<i64> = file_reader::read_vec_i64_from_file("input/day1.txt");
-  assert_eq!(4998565, fuel_counter_upper::total_fuel_needed_for_masses_including_fuel(masses));
-}
-
-fn day2_part1() {
-  let mut input_values: Vec<usize> = file_reader::read_vec_usize_from_file("input/day2.txt");
-  input_values[1] = 12;
-  input_values[2] = 2;
-  let result: Vec<usize> = intcode_computer::run_program(input_values);
-  assert_eq!(9581917, result[0]);
-}
-
-fn day2_part2() {
+fn solve() {
   let mut noun = 0;
   let mut verb = 0;
   let goal = 19690720;
